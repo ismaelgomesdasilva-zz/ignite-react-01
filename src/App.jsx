07647370bee2628a5bc2263b9b73669c25209ebb
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-07-27 10:21:50 -0300

    aula4: htmlwebpackplugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 function App() {
     return <h1>Hello World</h1>
--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,5 @@
 import React from 'react'
+import {render} from 'react-dom'
 import {App} from './App'
+
+render(<h1>test</h1>, document.getElementById('root'))
