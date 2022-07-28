16bfd5b8d59c853ce47f1570c84473c55da8be26
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-07-28 20:15:28 -0300

    aula7: development and production environment configuration. added scripts in package

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'development';
 module.exports ={
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment ?'development' : 'production',
+    devtool: isDevelopment? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output:{
        path: path.resolve(__dirname, 'dist'),
