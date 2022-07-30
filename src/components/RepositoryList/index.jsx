8cd5263b9c7ec693e7c9fe331d5a41f9aac8569a
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-07-30 19:27:07 -0300

    aula15: map repository data

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -2,11 +2,6 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./item";
 import './styles.scss'
 
-const repository = {
-  name: 'unform',
-  description:'form in react',
-  link:'https://github.com/ismaelgomesdasilva'
-}
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([])
   useEffect(() =>{
@@ -18,10 +13,9 @@ export function RepositoryList() {
     <section>
       <h1>Lista de repositorio</h1>
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {
+        repositories.map(repo =>(<RepositoryItem key={repo.name} repository={repo}/>) )
+        }
       </ul>
     </section>
   )
--- src/components/RepositoryList/item.jsx
+++ src/components/RepositoryList/item.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props){
     return(
         <li>
-          <strong>{props.repository.name?? "default"}</strong>
+          <strong>{props.repository.name}</strong>
           <p>{props.repository.description}</p>
-          <a href={props.repository.link}>Acessar repositorio</a>
+          <a href={props.repository.html_url}>Acessar repositorio</a>
         </li>
     )
 }
