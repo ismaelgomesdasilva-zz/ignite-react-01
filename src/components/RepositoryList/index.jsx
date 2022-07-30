03c5a3b6cd74af342e22d2b8311ea095453a5e55
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-07-30 19:16:40 -0300

    aula14: useEffect and request github api

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./item";
 import './styles.scss'
+
 const repository = {
   name: 'unform',
   description:'form in react',
   link:'https://github.com/ismaelgomesdasilva'
 }
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+  useEffect(() =>{
+    fetch('https://api.github.com/users/ismaelgomesdasilva/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  },[])
   return (
     <section>
       <h1>Lista de repositorio</h1>
